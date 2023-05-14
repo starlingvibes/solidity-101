c29d719f553f94ff070ba24c55aeab6dbbd099d3
starlingvibes <starlingroot@protonmail.com>
2023-05-14 15:40:17 +0100

    feat(): restrict access to owner

--- contracts/structs.sol
+++ contracts/structs.sol
@@ -18,8 +18,14 @@ pragma solidity 0.5.1;
 
 contract MyContract {
     uint public peopleCount = 0;
-
     mapping(uint => Person) public people;
+
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
 
     struct Person {
         uint _id;
@@ -27,8 +33,16 @@ contract MyContract {
         string _lastName;
     }
 
-    function addPerson(string memory _firstName, string memory _lastName) public {
-        peopleCount += 1;
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function addPerson(string memory _firstName, string memory _lastName) public onlyOwner {
+        incrementCount();
         people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
+
+    function incrementCount() internal {
+        peopleCount += 1;
     }
 }
