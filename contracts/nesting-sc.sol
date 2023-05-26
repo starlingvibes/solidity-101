f15e337180ddbf7cf304a47e51baab2842a549cb
starlingvibes <starlingroot@protonmail.com>
2023-05-26 12:09:58 +0100

    chore(): nesting smart contracts

---
+++ contracts/nesting-sc.sol
@@ -0,0 +1,30 @@
+pragma solidity 0.5.1;
+
+contract ERC20Token {
+    string public name;
+    mapping(address => uint256) public balances;
+
+    function mint() public {
+        balances[tx.origin] ++;
+    }
+}
+
+contract MyContract {
+    address payable wallet;
+    address public token;
+
+    constructor(address payable _wallet, address _token) public {
+        wallet = _wallet;
+        token = _token;
+    }
+
+    function() external payable {
+        buyToken();
+    }
+
+    function buyToken() public payable {
+        ERC20Token _token = ERC20Token(address(token));
+        _token.mint();
+        wallet.transfer(msg.value);
+    }   
+}
